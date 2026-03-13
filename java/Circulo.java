f1c69eafb824b22a283705bced17c28f7ac24a95
Rafael Knebel <rafael.knebel@estudante.uffs.edu.br>
2026-03-13 15:39:25 -0300

    Add files via upload

---
+++ java/Circulo.java
@@ -0,0 +1,13 @@
+public class Circulo {
+    double raio;
+    
+    double calculaArea() {
+        double area = 3.14 * raio * raio;
+        return area;
+    }
+
+
+
+
+
+}
---
+++ java/Triangulo.java
@@ -0,0 +1,14 @@
+public class Triangulo {
+    double base, altura;
+
+    double calculaArea{
+        double area = (base*altura) / 2;
+        
+        return area;
+    }
+
+
+
+
+
+}
